c5b7cdf28c2766b7433f913165e66db052784bca
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-04 12:32:19 +0200

    complete snack

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -3,24 +3,45 @@
 
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 
-Olimpia Milano - Cantù | 55-60
  -->
 
 <?php
 
 $teams = [
-   'home' => [
-      'Olimpia Milano' => ['points' => '55'],
-      'Segafredo Bologna' => ['points' => '87'],
+   [
+      'home' => 'Olimpia Milano',
+      'h_points' => '55',
+      'guest' => 'Cantù',
+      'g_points' => '60',
    ],
-   'guests' => [
-      'Cantù' => ['points' => '60'],
-      'Napoli' => ['points' => '78'],
+   [
+      'home' => 'Segafredo Bologna',
+      'h_points' => '89',
+      'guest' => 'Sacfati Basket',
+      'g_points' => '75',
+   ],
+   [
+      'home' => 'Reyer Venezia',
+      'h_points' => '58',
+      'guest' => 'New Basket Brindisi',
+      'g_points' => '48',
+   ],
+   [
+      'home' => 'Dinamo assari',
+      'h_points' => '91',
+      'guest' => 'Treviso Basket',
+      'g_points' => '90',
    ],
 ];
 
-var_dump($teams);
+/* var_dump($teams); */
 
+foreach ($teams as $match) {
+   /*    var_dump($match); */
 
+   echo $match['home'] . ' - ' . $match['guest'] . ' | ' . $match['h_points'] . '-' . $match['g_points'];
+
+   echo '|###|';
+}
 
 ?>
